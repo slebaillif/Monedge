6695faa4678bdc8ab35909db2c8517da5662d9d4
slebaillif@gmail.com <slebaillif@gmail.com>
2017-09-19 11:13:45 +0100

    formatting

--- web/static/js/basic-bar-chart.js
+++ web/static/js/basic-bar-chart.js
@@ -10,56 +10,56 @@ export function renderChart(data) {
     bottom: 30
   };
 
-  svg.attr('width', window.innerWidth-200);
-  svg.attr('height', window.innerHeight-100);
+  svg.attr('width', window.innerWidth - 200);
+  svg.attr('height', window.innerHeight - 100);
 
-  var width = window.innerWidth - margin.left - margin.right-200;
-  var height = window.innerHeight - margin.top - margin.bottom-100;
+  var width = window.innerWidth - margin.left - margin.right - 200;
+  var height = window.innerHeight - margin.top - margin.bottom - 100;
 
-  var   chart = svg.append('g')
-  .attr('width', width)
-  .attr('height', height)
-  .attr('transform', `translate(${margin.left}, ${margin.top})`);
-  chart.attr('width', window.innerWidth-200)
-  .attr('height', window.innerHeight-100);
+  var chart = svg.append('g')
+    .attr('width', width)
+    .attr('height', height)
+    .attr('transform', `translate(${margin.left}, ${margin.top})`);
+  chart.attr('width', window.innerWidth - 200)
+    .attr('height', window.innerHeight - 100);
 
   const x = d3.scaleBand()
-  .domain(data.map(d => d.letter))
-  .rangeRound([50, window.innerWidth - 250])
-  .padding(0.1);
+    .domain(data.map(d => d.letter))
+    .rangeRound([50, window.innerWidth - 250])
+    .padding(0.1);
 
   const y = d3.scaleLinear()
-  .domain([0, d3.max(data, d => d.presses)])
-  .range([window.innerHeight - 150, 0]);
+    .domain([0, d3.max(data, d => d.presses)])
+    .range([window.innerHeight - 150, 0]);
 
   const xAxis = d3.axisBottom().scale(x);
   const yAxis = d3.axisLeft().scale(y);
 
   chart.append('g')
-  .attr('class', 'axis')
-  .attr('transform', `translate(0, ${window.innerHeight - 150})`)
-  .call(xAxis);
+    .attr('class', 'axis')
+    .attr('transform', `translate(0, ${window.innerHeight - 150})`)
+    .call(xAxis);
 
   chart.append('g')
-  .attr('class', 'axis')
-  .attr('transform', 'translate(50, 0)')
-  .call(yAxis);
+    .attr('class', 'axis')
+    .attr('transform', 'translate(50, 0)')
+    .call(yAxis);
 
   chart.selectAll('rect')
-  .data(data)
-  .enter()
-  .append('rect')
-  .attr('class', 'bar')
-  .attr('x', d => x(d.letter))
-  .attr('y', window.innerHeight - 150)
-  .attr('width', x.bandwidth())
-  .attr('height', 0)
-  .transition()
-  .delay((d, i) => i * 20)
-  .duration(800)
-  .attr('y', d => y(d.presses))
-  .attr('height', d =>
-  (window.innerHeight - 150) - y(d.presses));
+    .data(data)
+    .enter()
+    .append('rect')
+    .attr('class', 'bar')
+    .attr('x', d => x(d.letter))
+    .attr('y', window.innerHeight - 150)
+    .attr('width', x.bandwidth())
+    .attr('height', 0)
+    .transition()
+    .delay((d, i) => i * 20)
+    .duration(800)
+    .attr('y', d => y(d.presses))
+    .attr('height', d =>
+      (window.innerHeight - 150) - y(d.presses));
 }
 
 export function renderStackedChart(data) {
@@ -67,44 +67,66 @@ export function renderStackedChart(data) {
 
   svg.attr('width', 600);
   svg.attr('height', 400);
-  
-  var margin = {top: 20, right: 20, bottom: 30, left: 40};
+
+  var margin = {
+    top: 20,
+    right: 20,
+    bottom: 30,
+    left: 40
+  };
   var width = +svg.attr("width") - margin.left - margin.right;
   var height = +svg.attr("height") - margin.top - margin.bottom;
   var g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var x = d3.scaleBand().rangeRound([0, width -150]).paddingInner(0.05).align(0.1);
-var y = d3.scaleLinear().rangeRound([height, 0]);
-var z = d3.scaleOrdinal(d3.schemeCategory20);
+  var x = d3.scaleBand().rangeRound([0, width - 150]).paddingInner(0.05).align(0.1);
+  var y = d3.scaleLinear().rangeRound([height, 0]);
+  var z = d3.scaleOrdinal(d3.schemeCategory20);
 
 
-data.sort(function(a, b) { return b.total - a.total; });
-x.domain(data.map(function(d) { return d.month; }));
-y.domain([0, d3.max(data, function(d) { return d.total; })]).nice();
-z.domain(keys);
+  data.sort(function (a, b) {
+    return b.total - a.total;
+  });
+  x.domain(data.map(function (d) {
+    return d.month;
+  }));
+  y.domain([0, d3.max(data, function (d) {
+    return d.total;
+  })]).nice();
+  z.domain(keys);
 
-g.append("g")
-  .selectAll("g")
-  .data(d3.stack().keys(keys)(data))
-  .enter().append("g")
-    .attr("fill", function(d) { return z(d.key); })
-  .selectAll("rect")
-  .data(function(d) { return d; })
-  .enter().append("rect")
-    .attr("x", function(d) { return x(d.data.month); })
-    .attr("y", function(d) { return y(d[1]); })
-    .attr("height", function(d) { console.log("height "+d[0] +" - " + d[1]); return y(d[0]) - y(d[1]); })
+  g.append("g")
+    .selectAll("g")
+    .data(d3.stack().keys(keys)(data))
+    .enter().append("g")
+    .attr("fill", function (d) {
+      return z(d.key);
+    })
+    .selectAll("rect")
+    .data(function (d) {
+      return d;
+    })
+    .enter().append("rect")
+    .attr("x", function (d) {
+      return x(d.data.month);
+    })
+    .attr("y", function (d) {
+      return y(d[1]);
+    })
+    .attr("height", function (d) {
+      console.log("height " + d[0] + " - " + d[1]);
+      return y(d[0]) - y(d[1]);
+    })
     .attr("width", x.bandwidth());
 
-g.append("g")
+  g.append("g")
     .attr("class", "axis")
     .attr("transform", "translate(0," + height + ")")
     .call(d3.axisBottom(x));
 
-g.append("g")
+  g.append("g")
     .attr("class", "axis")
     .call(d3.axisLeft(y).ticks(null, "s"))
-  .append("text")
+    .append("text")
     .attr("x", 2)
     .attr("y", y(y.ticks().pop()) + 0.5)
     .attr("dy", "0.32em")
@@ -113,26 +135,30 @@ g.append("g")
     .attr("text-anchor", "start")
     .text("Spending (£)");
 
-var legend = g.append("g")
+  var legend = g.append("g")
     .attr("font-family", "sans-serif")
     .attr("font-size", 10)
     .attr("text-anchor", "end")
-  .selectAll("g")
-  .data(keys.slice().reverse())
-  .enter().append("g")
-    .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+    .selectAll("g")
+    .data(keys.slice().reverse())
+    .enter().append("g")
+    .attr("transform", function (d, i) {
+      return "translate(0," + i * 20 + ")";
+    });
 
-legend.append("rect")
-    .attr("x", width )
+  legend.append("rect")
+    .attr("x", width)
     .attr("width", 19)
     .attr("height", 19)
     .attr("fill", z);
 
-legend.append("text")
+  legend.append("text")
     .attr("x", width - 24)
     .attr("y", 9.5)
     .attr("dy", "0.32em")
-    .text(function(d) { return d; });
+    .text(function (d) {
+      return d;
+    });
 
 
 }
